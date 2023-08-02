5e190f16e0bc31b54e4a43e80bf66f15de4b68db
tranngocphien <tranngocphien0309@gmail.com>
2023-08-02 15:37:59 +0700

    remove unused file

--- lib/app.dart
+++ lib/app.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: themeData,
       getPages: AppPages.pages,
       initialRoute: AppRoutes.splash,
